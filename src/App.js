b46c064581618a3c2b1390d18aa3d4b64e7d00ea
Eric <emzhang@umich.edu>
2024-01-22 00:57:42 -0500

    fixed typo

--- src/App.js
+++ src/App.js
@@ -107,7 +107,25 @@ const setType = (type)=>{
       
       <Textinput handleFileChange={handleFileChange} inputType ={inputType} coords = {Coords} path = {Path} fileContent={fileContent}></Textinput>
       <Manualinput inputType = {inputType}></Manualinput>
-      
+      <footer className='margin-left'>
+						<section>
+							<h2>Contact Me</h2>
+							<dl className="alt">
+								<dt>Location</dt>
+								<dd>Ann Arbor, MI</dd>
+								<dt>Phone</dt>
+								<dd>734-546-0902</dd>
+								<dt>Email</dt>
+								<dd><a href="#">emzhang@umich.edu</a></dd>
+							</dl>
+							<ul className="icons">
+							<li><a href="https://www.linkedin.com/in/eric-zhang-75b165251/" target="_blank" className="icon"><img src="linkedin.png" className ="header-icon" alt="LinkedIn logo"></img></a></li>
+							<li><a href="https://github.com/Ericmzhang" target="_blank" className="icon"><img src="github.png" className ="header-icon" alt="GitHub logo"></img></a></li>
+							<li><a href="Eric_Zhang_Resume.pdf" target="_blank" className="icon"><img src="resume.png" className ="header-icon" alt="PDF logo"></img></a></li>
+						</ul>
+						</section>
+						
+					</footer>
       
       
     </div>
